650b90ea526f2fc95d60068775b783db22160842
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-29 20:39:05 -0300

    Implement IsACenteredArray challenge method

--- CodeChallenges/Maharishi/Sample1.cs
+++ CodeChallenges/Maharishi/Sample1.cs
@@ -1,12 +1,25 @@
-﻿using System;
-
-namespace CodeChallenges.Maharishi
+﻿namespace CodeChallenges.Maharishi
 {
     public class MaharishiSample1
     {
         public static int IsACenteredArray(int[] sampleArray)
         {
-            throw new NotImplementedException();
+            var lenght = sampleArray?.Length;
+            if (lenght is null || lenght % 2 == 0)
+            {
+                return 0;
+            }
+            
+            var middleIndex = sampleArray.Length/2;
+            var middleValue = sampleArray[middleIndex];
+
+            for (var i = 0; i < lenght; i++)
+            {
+                if (sampleArray[i] <= middleValue && i != middleIndex)
+                    return 0;
+            }
+
+            return 1;
         }
     }
 }
