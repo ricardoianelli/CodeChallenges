b0dd897f107450f875373953087899bc4b64da8e
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-29 20:03:22 -0300

    Create basic project

---
+++ CodeChallenges/Maharishi/Sample1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.Maharishi
+{
+    public class MaharishiSample1
+    {
+        public static int IsACenteredArray(int[] sampleArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Tests/Maharishi/Sample1.cs
@@ -0,0 +1,22 @@
+﻿using Xunit;
+using CodeChallenges.Maharishi;
+using FluentAssertions;
+
+namespace Tests.Maharishi
+{
+    public class Sample1
+    {
+        [Theory]
+        [InlineData(new []{1, 2, 3, 4, 5}, 0)]
+        [InlineData(new []{3, 2, 1, 4, 5}, 1)]
+        [InlineData(new []{3, 2, 1, 4, 1}, 0)]
+        [InlineData(new []{1, 2, 3, 4}, 0)]
+        [InlineData(new int[]{}, 0)]
+        [InlineData(new []{10}, 1)]
+        public void Maharishi1(int[] input, int expectedOutput)
+        {
+            var output = MaharishiSample1.IsACenteredArray(input);
+            output.Should().Be(expectedOutput);
+        }
+    }
+}
