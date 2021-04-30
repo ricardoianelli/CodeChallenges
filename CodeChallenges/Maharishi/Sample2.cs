d327c3d06dd1f7bab1b6bdae8b4f5ddf355aa593
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-29 21:39:28 -0300

    Create Maharishi.Sample2 method signature

---
+++ CodeChallenges/Maharishi/Sample2.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.Maharishi
+{
+    public class Sample2
+    {
+        public static int DifferenceOfSums(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
