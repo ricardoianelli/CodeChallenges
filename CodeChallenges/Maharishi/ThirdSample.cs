90b2745310ff5e3b011ab714c433efa75c0b1333
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-29 22:12:31 -0300

    Create Maharishi third sample signature

---
+++ CodeChallenges/Maharishi/ThirdSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.Maharishi
+{
+    public class ThirdSample
+    {
+        public static char[] GetCharsFromArray(char[] charArray, int startIndex, int charsToTake)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
