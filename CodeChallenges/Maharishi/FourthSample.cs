faccfd1f06b77df0f478005d8bbc89ade0175491
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-30 19:17:52 -0300

    Create Maharishi fourth sample signature

---
+++ CodeChallenges/Maharishi/FourthSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.Maharishi
+{
+    public class FourthSample
+    {
+        public static int ReverseNumber(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
