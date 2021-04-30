8e3390a5661dbf3b1e4622f1728e1d8c973fac4b
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-30 20:30:31 -0300

    Implement Maharishi fourth sample code

--- CodeChallenges/Maharishi/FourthSample.cs
+++ CodeChallenges/Maharishi/FourthSample.cs
@@ -6,7 +6,34 @@ namespace CodeChallenges.Maharishi
     {
         public static int ReverseNumber(int number)
         {
-            throw new NotImplementedException();
+            if (number == 0)
+                return 0;
+            
+            var negativeNumber = number < 0;
+            if (negativeNumber)
+                number *= -1;
+            
+            var outputNumber = 0;
+            var powerOfTen = (int) Math.Log10(number);
+
+            do
+            {
+                var rightmostNumber = number % 10;
+                var currentBase = (int) Math.Pow(10, powerOfTen);
+                var currentPart = rightmostNumber * currentBase;
+
+                outputNumber += currentPart;
+                number /= 10;
+                powerOfTen--;
+            } 
+            while (powerOfTen > 0);
+
+            outputNumber += number;
+
+            if (negativeNumber)
+                outputNumber *= -1;
+            
+            return outputNumber;
         }
     }
 }
