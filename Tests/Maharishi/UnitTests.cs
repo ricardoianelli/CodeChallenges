749050c9cec54260cc6efc408808602d80c4b093
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-04-29 21:46:11 -0300

    Create Maharishi second sample unit tests

--- Tests/Maharishi/UnitTests.cs
+++ Tests/Maharishi/UnitTests.cs
@@ -19,5 +19,22 @@ namespace Tests.Maharishi
             var output = FirstSample.IsACenteredArray(input);
             output.Should().Be(expectedOutput);
         }
+        
+        [Theory]
+        [InlineData(new []{1}, 1)]
+        [InlineData(new []{1, 2}, -1)]
+        [InlineData(new []{1, 2, 3}, 2)]
+        [InlineData(new []{1, 2, 3, 4}, -2)]
+        [InlineData(new []{3, 3, 4, 4}, -2)]
+        [InlineData(new []{3, 2, 3, 4}, 0)]
+        [InlineData(new []{4, 1, 2, 3}, -2)]
+        [InlineData(new []{1, 1}, 2)]
+        [InlineData(new int[]{}, 0)]
+        [InlineData(null, 0)]
+        public void DifferenceOfSums_GivenAnInput_ShouldMatchExpectedOutput(int[] input, int expectedOutput)
+        {
+            var output = SecondSample.DifferenceOfSums(input);
+            output.Should().Be(expectedOutput);
+        }
     }
 }
