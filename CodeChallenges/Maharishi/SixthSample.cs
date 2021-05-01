a21590afa4e2c581fbf1b5c02af360e9301ec85a
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-05-01 12:46:11 -0300

    Create Maharishi sixth sample implementation

--- CodeChallenges/Maharishi/SixthSample.cs
+++ CodeChallenges/Maharishi/SixthSample.cs
@@ -6,7 +6,29 @@ namespace CodeChallenges.Maharishi
     {
         public static int GetPointOfEquilibrium(int[] array)
         {
-            throw new NotImplementedException();
+            if (array == null || array.Length < 3)
+                return -1;
+
+            var rightSum = 0;
+            var leftSum = 0;
+
+            for (int l = 1; l < array.Length; l++)
+            {
+                leftSum += array[l-1];
+                for (int r = array.Length-1; r > l; r--)
+                {
+                    rightSum += array[r];
+                }
+
+                if (leftSum > rightSum)
+                    break; 
+                if (leftSum == rightSum)
+                    return l;
+                
+                rightSum = 0;
+            }
+
+            return -1;
         }
     }
 }
