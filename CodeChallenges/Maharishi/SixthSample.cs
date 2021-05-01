fb792a8a2242fe3c94c1e96cf071fed5a8d5954c
Ricardo Ianelli <ricardo.ianelli@stone.com.br>
2021-05-01 10:47:14 -0300

    Create Maharishi sixth sample signature

---
+++ CodeChallenges/Maharishi/SixthSample.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.Maharishi
+{
+    public class SixthSample
+    {
+        public static int GetPointOfEquilibrium(int[] array)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
