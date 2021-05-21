03c19ef629680e711583c5a843c87617d83f6de1
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 16:47:34 -0300

    Create RaiseNumber implementation

---
+++ CodeChallenges/General/Numbers.cs
@@ -0,0 +1,24 @@
+﻿namespace CodeChallenges.General
+{
+    public static class Numbers
+    {
+        public static int RaiseNumber(int n, int power)
+        {
+            switch (n)
+            {
+                case 1:
+                    return 1;
+                case <= 0:
+                    return 0;
+            }
+
+            var result = 1;
+            for (int i = 0; i < power; i++)
+            {
+                result *= n;
+            }
+
+            return result;
+        }
+    }
+}
