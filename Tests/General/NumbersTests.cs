99256976bafbb7c12698e2f9eeb9fdae5f919edc
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 16:47:27 -0300

    Create Unit tests for RaiseNumber method

---
+++ Tests/General/NumbersTests.cs
@@ -0,0 +1,23 @@
+﻿using CodeChallenges.General;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.General
+{
+    public class NumbersTests
+    {
+        [Theory]
+        [InlineData(0, 3, 0)]
+        [InlineData(1, 0, 1)]
+        [InlineData(1, 1, 1)]
+        [InlineData(1, 5, 1)]
+        [InlineData(2, 0, 1)]
+        [InlineData(2, 1, 2)]
+        [InlineData(2, 2, 4)]
+        [InlineData(3, 4, 81)]
+        public void RaiseNumber_GivenANumberAndAPower_ShouldReturnTheNumberToThePower(int n, int power, int expected)
+        {
+            Numbers.RaiseNumber(n, power).Should().Be(expected);
+        }
+    }
+}
