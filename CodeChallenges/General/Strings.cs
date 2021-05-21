78da3afd1dd5ef1a814e94159b67dd03d7eabf0a
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 17:24:40 -0300

    Create CountCharAppearance implementation

---
+++ CodeChallenges/General/Strings.cs
@@ -0,0 +1,19 @@
+﻿namespace CodeChallenges.General
+{
+    public class Strings
+    {
+        public static int CountCharAppearance(string stringToSearch, char charToLookUp)
+        {
+            if (stringToSearch is null)
+                return 0;
+
+            var count = 0;
+            for (int i = 0; i < stringToSearch.Length; i++)
+            {
+                if (stringToSearch[i] == charToLookUp)
+                    count++;
+            }
+            return count;
+        }
+    }
+}
