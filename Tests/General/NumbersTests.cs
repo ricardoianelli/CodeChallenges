d87e67a0ea269c1526a36c8d917337ebb4ab7759
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 17:00:04 -0300

    Improve RaiseNumber tests to accept double and negative values

--- Tests/General/NumbersTests.cs
+++ Tests/General/NumbersTests.cs
@@ -7,6 +7,10 @@ namespace Tests.General
     public class NumbersTests
     {
         [Theory]
+        [InlineData(2, -2, 0.25)]
+        [InlineData(2, -1, 0.5)]
+        [InlineData(1, -1, 1)]
+        [InlineData(0, -1, 0)]
         [InlineData(0, 3, 0)]
         [InlineData(1, 0, 1)]
         [InlineData(1, 1, 1)]
@@ -15,7 +19,7 @@ namespace Tests.General
         [InlineData(2, 1, 2)]
         [InlineData(2, 2, 4)]
         [InlineData(3, 4, 81)]
-        public void RaiseNumber_GivenANumberAndAPower_ShouldReturnTheNumberToThePower(int n, int power, int expected)
+        public void RaiseNumber_GivenANumberAndAPower_ShouldReturnTheNumberToThePower(double n, int power, double expected)
         {
             Numbers.RaiseNumber(n, power).Should().Be(expected);
         }
