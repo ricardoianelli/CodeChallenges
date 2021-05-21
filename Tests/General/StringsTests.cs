684112a0726590d2dfef471c9af4728983d1b965
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 17:24:33 -0300

    Create CountCharAppearance unit tests

---
+++ Tests/General/StringsTests.cs
@@ -0,0 +1,20 @@
+﻿using CodeChallenges.General;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.General
+{
+    public class StringsTests
+    {
+        [Theory]
+        [InlineData("ricardo", 'i', 1)]
+        [InlineData("ricardo", 'r', 2)]
+        [InlineData("ricardo", 'f', 0)]
+        [InlineData("", 'f', 0)]
+        [InlineData(null, 'f', 0)]
+        public void CountCharAppearance_GivenAStringAndAChar_ShouldReturnCorrectCount(string s, char c, int expected)
+        {
+            Strings.CountCharAppearance(s, c).Should().Be(expected);
+        }
+    }
+}
