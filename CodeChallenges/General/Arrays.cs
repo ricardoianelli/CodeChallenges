85ef79a0a73688601ce180a97dff8ffc316450ca
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 12:38:42 -0300

    Create ReverseArray implementation

--- CodeChallenges/General/Arrays.cs
+++ CodeChallenges/General/Arrays.cs
@@ -6,7 +6,21 @@ namespace CodeChallenges.General
     {
         public static int[] ReverseArray(int[] inputArray)
         {
-            throw new NotImplementedException();
+            if (inputArray is null || inputArray.Length < 2)
+                return inputArray;
+            
+            int lastPickedValue;
+            int oppositeIndex;
+            var lastIndex = inputArray.Length - 1;
+            for (int i = 0; i < inputArray.Length / 2; i++)
+            {
+                lastPickedValue = inputArray[i];
+                oppositeIndex = lastIndex - i;
+                inputArray[i] = inputArray[oppositeIndex];
+                inputArray[oppositeIndex] = lastPickedValue;
+            }
+
+            return inputArray;
         }
     }
 }
