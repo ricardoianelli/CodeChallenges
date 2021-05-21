a110cdff6a58d4c5201ff95fcfc0e55efe47d1cf
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 12:28:19 -0300

    Create ReverseArray unit tests

---
+++ Tests/General/ArraysTests.cs
@@ -0,0 +1,19 @@
+﻿using CodeChallenges.General;
+using FluentAssertions;
+using Xunit;
+
+namespace Tests.General
+{
+    public class ArraysTests
+    {
+        [Fact]
+        public void ReverseArray_GivenAnIntegerArray_ReturnTheArrayReversed()
+        {
+            var inputArray = new[] {1, 2, 3, 4, 5, 6};
+            var expected = new[] {6, 5, 4, 3, 2, 1};
+
+            var result = Arrays.ReverseArray(inputArray);
+            result.Should().Equal(expected);
+        }
+    }
+}
