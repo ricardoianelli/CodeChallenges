567aff2b2820cf5bd2e496c396a6e71d563da1b0
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 12:38:28 -0300

    Add new case for ReverseArray unit tests

--- Tests/General/ArraysTests.cs
+++ Tests/General/ArraysTests.cs
@@ -12,6 +12,7 @@ namespace Tests.General
         [InlineData(new[] {1}, new[] {1})]
         [InlineData(new[] {1, 2}, new[] {2, 1})]
         [InlineData(new[] {1, 5, 3, 6, 1, 3}, new[] {3, 1, 6, 3, 5, 1})]
+        [InlineData(new[] {23, 5, 1, 0, -5}, new[] {-5, 0, 1, 5, 23})]
         public void ReverseArray_GivenAnIntegerArray_ReturnTheArrayReversed(int[] inputArray, int[] expected)
         {
             var result = Arrays.ReverseArray(inputArray);
