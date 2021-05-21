fa096289ea57bce4a6d6bb044e45473b3b6073be
ricardoianelli <ricardoianelli@hotmail.com>
2021-05-21 12:27:52 -0300

    Create ReverseArray method signature

---
+++ CodeChallenges/General/Arrays.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeChallenges.General
+{
+    public static class Arrays
+    {
+        public static int[] ReverseArray(int[] inputArray)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
